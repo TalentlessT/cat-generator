f4cf3d842312ac1585bf5e9b175abc8836f3aeac
THANOS_CAR <jan.342003@seznam.cz>
2021-05-16 16:04:32 +0200

    Added some styling plus fixed mobile responsivity

--- src/CatGenerator.js
+++ src/CatGenerator.js
@@ -1,25 +1,29 @@
-import {useState} from 'react';
+import { useState } from 'react';
 
 const CatGenerator = () => {
 
-    const [img, setImg] = useState();
+    const [img, setImg] = useState("https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png");
 
-    function fetch_data(){
-    fetch("https://api.thecatapi.com/v1/images/search")
-    .then(res => {
-        return res.json();
-    }).then(result => {
-        setImg(result[0].url);
-    })
+    function fetch_data() {
+        fetch("https://api.thecatapi.com/v1/images/search")
+            .then(res => {
+                return res.json();
+            }).then(result => {
+                setImg(result[0].url);
+            })
     }
 
-    return ( 
+    return (
         <div>
-            <h1>cat generator</h1>
-            <img src={img}></img>
-            <button onClick={fetch_data}>new one</button>
+            <h1>cat image generator</h1>
+                <div className="cat-image">
+                    <img src={img} alt="random-cat" />
+                </div>
+                <div className="new-button">
+                    <button onClick={fetch_data}>new cat</button>
+                </div>
         </div>
-     );
+    );
 }
- 
+
 export default CatGenerator;
